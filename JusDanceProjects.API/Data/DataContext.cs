d68b8483807ee47182254832b14eac0459c88769
Jvzuijlen <jvzuijlen@hotmail.com>
2019-11-15 13:59:46 +0100

    Initial Commit

---
+++ JusDanceProjects.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using JusDanceProjects.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JusDanceProjects.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
