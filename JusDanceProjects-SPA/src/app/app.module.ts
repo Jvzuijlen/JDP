1e5df3e9a3c3330da62e48da71c7256ecb4e9f0d
Jvzuijlen <jvzuijlen@hotmail.com>
2019-11-17 21:52:39 +0100

    Started on NavBar

--- JusDanceProjects-SPA/src/app/app.module.ts
+++ JusDanceProjects-SPA/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule
    ],
    providers: [],
    bootstrap: [
