1c47805b02931956ca613576f4dcc83aa8be6c03
Jvzuijlen <jvzuijlen@hotmail.com>
2019-11-19 11:08:36 +0100

    Nav + Sidebar

--- JusDanceProjects-SPA/src/app/app.module.ts
+++ JusDanceProjects-SPA/src/app/app.module.ts
@@ -1,26 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule
+} from '@angular/material';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { NavLoginComponent } from './nav/nav-login/nav-login.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      NavLoginComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
-      MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule
+      FlexLayoutModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatButtonModule,
+      MatListModule,
+      BrowserAnimationsModule,
+      NgbModule
    ],
    providers: [],
    bootstrap: [
       AppComponent
    ]
 })
-export class AppModule { }
+export class AppModule {}
