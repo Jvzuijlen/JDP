b01c33de7b8ec5c2b89fa3baeaae8f8dd5045c21
Jvzuijlen <jvzuijlen@hotmail.com>
2019-12-02 17:16:34 +0100

    Fixed datepicker

--- JusDanceProjects-SPA/src/app/app.component.ts
+++ JusDanceProjects-SPA/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { NgbDatepickerConfig, NgbCalendar, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbDatepickerConfig] // add NgbDatepickerConfig to the component providers
 })
 export class AppComponent {
   title = 'JusDanceProjects-SPA';
 
-  constructor() {}
+  constructor(config: NgbDatepickerConfig, calendar: NgbCalendar) {
+    // customize default values of datepickers used by this component tree
+    config.minDate = {year: 1900, month: 1, day: 1};
+    config.maxDate = calendar.getToday();
+
+    // days that don't belong to current month are not visible
+    config.outsideDays = 'hidden';
+  }
 }
